207bacedbce3a00ed7c7831e98a01c3638f1ee7e
DumDumGeniuss <dumdumgenius@gmail.com>
2017-04-30 12:42:04 +0800

    Connect Mongodb on both development and production mode

--- src/app.js
+++ src/app.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const chalk = require('chalk');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
 
 /* Loda config by dotenv */
 dotenv.load({ path: '.env' });
 
-console.log(process.env.MONGODB_URI);
-
+/* Set config */
 app.set('port', process.env.port);
+app.set('db_uri', process.env.MONGODB_URI || process.env.MONGODB_LAB_URI);
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello Wolrd!!!');
+/* connect to DB */
+mongoose.Promise = global.Promise;
+mongoose.connect(app.get('db_uri'));
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s Connecting to Mongodb failed, please check your mongodb', chalk.red('✗'));
+  process.exit();
+});
+mongoose.connection.on('connected', () => {
+  console.log('%s Successful connected to Mongodb', chalk.green('✓'));
 });
 
+
+app.get('/', (req, res) => {
+  res.status(200).send('Express Application Works Fine');
+});
+
+/* Start server */
 app.listen(app.get('port'), () => {
   console.log('%s Node server‘s running on port %s', chalk.green('✓'), app.get('port'));
 });
